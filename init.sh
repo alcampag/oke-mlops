59dfc8d3ffddf670ac123974aef890857c456da6
Alberto Campagna <alberto.campagna@oracle.com>
2024-06-26 08:40:28 +0200

    added automation scripts

--- init.sh
+++ init.sh
@@ -7,20 +7,19 @@ export PUB_LB_NSG_ID=$(tofu output -raw pub_lb_nsg_id)
 export OKE_REGION=$(tofu output -raw region)
 export OKE_ID=$(tofu output -raw oke_cluster_id)
 
-VALUES="
-{ service:
-  { annotations:
+rm -f istio-ingress-values.yaml
+cat <<EOF | tee istio-ingress-values.yaml
+service:
+  annotations:
     {
-      'oci.oraclecloud.com/load-balancer-type':'lb',
-      'service.beta.kubernetes.io/oci-load-balancer-shape':'flexible',
-      'service.beta.kubernetes.io/oci-load-balancer-shape-flex-min':'10',
-      'service.beta.kubernetes.io/oci-load-balancer-shape-flex-max':'10',
-      'oci.oraclecloud.com/oci-network-security-groups': $PUB_LB_NSG_ID,
-      'oci.oraclecloud.com/security-rule-management-mode':'None'
+      "oci.oraclecloud.com/load-balancer-type":"lb",
+      "service.beta.kubernetes.io/oci-load-balancer-shape":"flexible",
+      "service.beta.kubernetes.io/oci-load-balancer-shape-flex-min":"10",
+      "service.beta.kubernetes.io/oci-load-balancer-shape-flex-max":"10",
+      "oci.oraclecloud.com/oci-network-security-groups":"${PUB_LB_NSG_ID}",
+      "oci.oraclecloud.com/security-rule-management-mode":"None"
     }
-    }
-    }
-"
+EOF
 
 oci ce cluster create-kubeconfig --cluster-id $OKE_ID --file $HOME/.kube/config --region $OKE_REGION --token-version 2.0.0  --kube-endpoint PUBLIC_ENDPOINT
 
@@ -30,28 +29,28 @@ echo "Waiting for at least one node to be ready"
 until kubectl get nodes | grep -i "Ready"; do sleep 1 ;  done
 
 echo "Installing Istio ingress gateway"
-echo $VALUES | helm install istio-ingressgateway istio/gateway -f istio-ingress-values.yaml --version 1.20.5 -n istio-system || exit
+helm upgrade --install istio-ingressgateway istio/gateway -f istio-ingress-values.yaml --version 1.20.5 -n istio-ingress --create-namespace || exit
 
 echo "Installing Knative"
 export KNATIVE_VERSION="v1.14.1"
 
 # Install CRDs
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-${KNATIVE_VERSION}/serving-crds.yaml
+while ! kubectl apply -f https://github.com/knative/serving/releases/download/knative-${KNATIVE_VERSION}/serving-crds.yaml; do echo "Retrying installing Knative CRDs"; sleep 10; done
 
 # Install core Knative Serving
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-${KNATIVE_VERSION}/serving-core.yaml
+while ! kubectl apply -f https://github.com/knative/serving/releases/download/knative-${KNATIVE_VERSION}/serving-core.yaml; do echo "Retrying installing Knative Serving"; sleep 10; done
 
 # Install Knative Istio Controller
-kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-${KNATIVE_VERSION}/net-istio.yaml
+while ! kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-${KNATIVE_VERSION}/net-istio.yaml; do echo "Retrying installing Knative Istio Controller"; sleep 10; done
 
 # Install sslip.io Job for default DNS
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-${KNATIVE_VERSION}/serving-default-domain.yaml
+while ! kubectl apply -f https://github.com/knative/serving/releases/download/knative-${KNATIVE_VERSION}/serving-default-domain.yaml; do echo "Retrying installing Knative Domain Job"; sleep 10; done
 
 echo "Installing KServe"
 export KSERVE_VERSION="v0.13.0"
 
 # Install KServe
-kubectl apply -f https://github.com/kserve/kserve/releases/download/${KSERVE_VERSION}/kserve.yaml
+while ! kubectl apply -f https://github.com/kserve/kserve/releases/download/${KSERVE_VERSION}/kserve.yaml; do echo "Retrying installing KServe"; sleep 10; done
 
 # Install KServe Built-in ClusterServingRuntimes
-kubectl apply -f https://github.com/kserve/kserve/releases/download/${KSERVE_VERSION}/kserve-cluster-resources.yaml
+while ! kubectl apply -f https://github.com/kserve/kserve/releases/download/${KSERVE_VERSION}/kserve-cluster-resources.yaml; do echo "Retrying installing KServe Cluster Resources"; sleep 10; done
