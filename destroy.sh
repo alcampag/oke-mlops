7f811c3bb2b7e29f20c236e4ecbad8293abb391d
Alberto Campagna <alberto.campagna@oracle.com>
2024-07-04 08:09:29 +0200

    added automation scripts

--- destroy.sh
+++ destroy.sh
@@ -4,6 +4,6 @@
 export OKE_COMPARTMENT=$(tofu output -raw compartment_id)
 export OKE_REGION=$(tofu output -raw region)
 
-helm uninstall istio-ingressgateway --wait --ignore-not-found -n istio-system
-helm uninstall oke-mlflow --wait --ignore-not-found -n mlflow
+helm uninstall istio-ingressgateway --wait -n istio-system || true
+helm uninstall oke-mlflow --wait -n mlflow || true
 tofu destroy -var="tenancy_ocid=$OCI_TENANCY" -var="region=$OKE_REGION" -var="compartment_ocid=$OKE_COMPARTMENT"
